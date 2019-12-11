62613388143ad165de4166f86c8718b80f55241d
Luisa Griesbach <grlu1017@hs-karlsruhe.de>
2019-12-11 14:52:09 +0100

    start

---
+++ app/src/main/java/grlu1017/info3/oepnvnavigator/ui/send/SendFragment.java
@@ -0,0 +1,35 @@
+package grlu1017.info3.oepnvnavigator.ui.send;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import grlu1017.info3.oepnvnavigator.R;
+
+public class SendFragment extends Fragment {
+
+    private SendViewModel sendViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        sendViewModel =
+                ViewModelProviders.of(this).get(SendViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_send, container, false);
+        final TextView textView = root.findViewById(R.id.text_send);
+        sendViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
